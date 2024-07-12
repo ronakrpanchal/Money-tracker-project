02997bb2523b635e50a16a5e1331f95c8fde1481
ronakrpanchal <124129862+ronakrpanchal@users.noreply.github.com>
2024-07-12 16:15:02 +0530

    Add files via upload

--- Send_weekly_report.py
+++ Send_weekly_report.py
@@ -4,20 +4,24 @@ from email.mime.text import MIMEText
 import sqlite3
 import datetime
 
+#Credentials for sending mail through smtplib.
 my_email = "Sender's email"
 password = "your_password"
 email="Reciever's email"
 
+#Creating a database.
 db_path="your database path where you want/created the database"
 con = sqlite3.connect(db_path)
 c=con.cursor()
 
+#Calulate the last week's starting and ending dates
 today=datetime.datetime.today()
 sow=today-datetime.timedelta(days=today.weekday(),weeks=1)
 eow=sow+datetime.timedelta(days=6)
 sow=sow.strftime("%Y-%m-%d")
 eow=eow.strftime("%Y-%m-%d")
 
+#Getting the data from the database
 c.execute("""
           SELECT DATE , AMOUNT , DETAILS 
           FROM YOURFINANCE 
@@ -25,7 +29,30 @@ c.execute("""
           ORDER BY DATE
           """,(sow,eow))
 
+#fetches rows from the table 
 rows=c.fetchall()
+
+#fetches the data from the second last week
+sotw=today-datetime.timedelta(days=today.weekday(),weeks=2)
+eotw=sotw+datetime.timedelta(days=6)
+sotw=sotw.strftime("%Y-%m-%d")
+eotw=eotw.strftime("%Y-%m-%d")
+
+c.execute("""
+          SELECT DATE , AMOUNT , DETAILS 
+          FROM YOURFINANCE 
+          WHERE DATE BETWEEN ? AND ?
+          ORDER BY DATE
+          """,(sotw,eotw))
+
+sweek=c.fetchall()
+
+#Calculating the total amount spend or recieved during second week
+total_amount_2=0
+for i in range(len(sweek)):
+    total_amount_2+=sweek[i][1]
+
+#creates a table in html format and adds relevant info in the table
 if rows:
     report = """
     <html>
@@ -83,9 +110,33 @@ if rows:
         emoji="😌"
     else:
         emoji="🙂‍↕️"
+    ud=''
+    ml=''
+    if total_amount<0 and total_amount_2<0:
+        total_amount=-total_amount
+        total_amount_2=-total_amount_2
+        if total_amount>total_amount_2:
+            pr=(total_amount-total_amount_2)/total_amount
+            ud='🔺'
+            ml='more'
+        elif total_amount_2>total_amount:
+            pr=(total_amount_2-total_amount)/total_amount_2
+            ud='🔻'
+            ml='less'
+    elif total_amount>0 and total_amount_2>0:
+        if total_amount>total_amount_2:
+            pr=(total_amount-total_amount_2)/total_amount
+            ud='🔺'
+            ml='more'
+        elif total_amount_2>total_amount:
+            pr=(total_amount_2-total_amount)/total_amount_2
+            ud='🔻'
+            ml='less'
     report += f"""
     </table>
-    <p style="{total_amount_style}">You {sr} {total_amount_display} last week {emoji}.</p>
+    <p style="{total_amount_style}">You {sr} {total_amount_display} last week {emoji}.
+    {round(pr*100,2)}% {ud} {ml} than last week
+    </p>
     </body>
     </html>
     """
@@ -99,6 +150,7 @@ else:
     </html>
     """
 
+#Sending mail through smtplib.
 try:
     with smtplib.SMTP("smtp.gmail.com",port=587) as server:
         server.starttls()
